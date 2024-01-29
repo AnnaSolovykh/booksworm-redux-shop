defd208466241a360c1e5d7d37a894e888e98974
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-29 21:18:06 +0300

    fix fav books rendering on home page when logged out

--- src/Components/BooksComponents/Book.js
+++ src/Components/BooksComponents/Book.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import ChangeQuantity from "../Cart/ChangeQuantity"
-import { addItemToCart, getCartItems, updateQuantity } from "../Redux/cartSlice"
-import { setFavoriteStatus, getFavoriteStatus, fetchIsFavorite, addToFavoritesAsync, removeFromFavoritesAsync } from "../Redux/favoritesSlice";
+import ChangeQuantity from '../Cart/ChangeQuantity';
+import LoginModal from '../Others/LoginModal';
+import { addItemToCart, getCartItems, updateQuantity } from '../Redux/cartSlice';
+import { selectIsLoggedIn } from '../Redux/authenticationSlice';
+import { setFavoriteStatus, getFavoriteStatus, fetchIsFavorite, addToFavoritesAsync, removeFromFavoritesAsync } from '../Redux/favoritesSlice';
 import { ToastContainer, toast } from 'react-toastify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as fasFaHeart } from '@fortawesome/free-solid-svg-icons';
@@ -12,21 +14,27 @@ import { faHeart as farFaHeart } from '@fortawesome/free-regular-svg-icons';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Book = ({book}) => {
+    const [showText, setShowText] = useState(false);
+    const [quantity, setQuantity] = useState(1);
+    const [showLoginModal, setShowLoginModal] = useState(false);
 
-    const [showText, setShowText] = useState(false)
+    const dispatch = useDispatch();
+
     const showTextClick = (book) => {
         book.showMore = !book.showMore
         setShowText(!showText)
-    }
-
-    const dispatch = useDispatch();
-    const [quantity, setQuantity] = useState(1);
+    };
 
     const isFavorite = useSelector((state) =>
         getFavoriteStatus(state, book.id)
     );
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
     const toggleFavoriteStatus = () => {
+        if (!isLoggedIn) {
+            setShowLoginModal(true); 
+            return
+        }
         if (isFavorite) {
             dispatch(removeFromFavoritesAsync(book));
             dispatch(setFavoriteStatus({ bookId: book.id, isFavorite: false }));
@@ -37,9 +45,12 @@ const Book = ({book}) => {
     };
 
     useEffect(() => {
-        dispatch(fetchIsFavorite(book.id));
-    }, [dispatch, book.id]);
-
+        if (!isLoggedIn) {
+            isFavorite && dispatch(setFavoriteStatus({ bookId: book.id, isFavorite: false }));
+        } else {
+            dispatch(fetchIsFavorite(book.id));
+        }
+    }, [dispatch, book.id, isLoggedIn, isFavorite]);
     const cartItems = useSelector( getCartItems  )
 
     const booksInCart = cartItems.some ( item => item.id === book.id)
@@ -48,28 +59,28 @@ const Book = ({book}) => {
     const putToCart = () => {
         if (!booksInCart) {
             dispatch (addItemToCart ({ book, quantity })) 
-            const notify = () => toast.success("Added to your basket!", {
-                position: "top-right",
+            const notify = () => toast.success('Added to your basket!', {
+                position: 'top-right',
                 autoClose: 1000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                theme: "light",
+                theme: 'light',
             });
             notify()
         } else {
             dispatch (updateQuantity ( {book, quantity} ) )
-            const notify = () => toast.success("Added to your basket!", {
-                position: "top-right",
+            const notify = () => toast.success('Added to your basket!', {
+                position: 'top-right',
                 autoClose: 1000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                theme: "light",
+                theme: 'light',
             })
             notify()
         }
@@ -79,24 +90,25 @@ const Book = ({book}) => {
     return (
         <div>
 
-        <div className="book-box">
+        <div className='book-box'>
 
-            <div className="book-image-box">
-                <img className="book-image" src={`./${book.img}.jpeg`} alt="a book"/>
+            <div className='book-image-box'>
+                <img className='book-image' src={`./${book.img}.jpeg`} alt='a book'/>
             </div>
 
-            <div className="book-info-box">
-                <h2 className="book-name">{book.name}</h2>
-                <h4 className="book-author">{book.author}</h4>
-                <h3 className="book-price">$ {book.price}</h3> 
+            <div className='book-info-box'>
+                <h2 className='book-name'>{book.name}</h2>
+                <h4 className='book-author'>{book.author}</h4>
+                <h3 className='book-price'>$ {book.price}</h3> 
                 <ChangeQuantity quantity={quantity} setQuantity={setQuantity}/>   
-                <button className="add-to-cart-btn" onClick= { putToCart } >Add to cart</button>
-                <button className="toggle-favorite-btn" onClick={toggleFavoriteStatus}>
+                <button className='add-to-cart-btn' onClick= { putToCart } >Add to cart</button>
+                <button className='toggle-favorite-btn' onClick={toggleFavoriteStatus}>
                     <FontAwesomeIcon icon={isFavorite ? fasFaHeart : farFaHeart} />
                 </button>
+                {showLoginModal && <LoginModal onClose={() => setShowLoginModal(false)} />}
 
                 <ToastContainer 
-                    position="top-right"
+                    position='top-right'
                     autoClose={1000}
                     hideProgressBar={false}
                     newestOnTop={false}
@@ -105,22 +117,22 @@ const Book = ({book}) => {
                     pauseOnFocusLoss
                     draggable
                     pauseOnHover
-                    theme="light"
+                    theme='light'
             />
                 
             </div>
             </div>
 
-            <div className="description-box">
-                <h4 className="book-author">SINOPSIS</h4>
-                <p className="book-description">{book.showMore ? book.description: book.description.substring(0, 350)+ "..."}
-                <button className="show-more-btn" onClick={ () => showTextClick(book)}>{book.showMore ? "Show less" : "Show more"}</button>
+            <div className='description-box'>
+                <h4 className='book-author'>SINOPSIS</h4>
+                <p className='book-description'>{book.showMore ? book.description: book.description.substring(0, 350)+ '...'}
+                <button className='show-more-btn' onClick={ () => showTextClick(book)}>{book.showMore ? 'Show less' : 'Show more'}</button>
                 </p>
             </div>
             
         
 
-        <div className="line"></div>
+        <div className='line'></div>
         </div>
     )
 }
